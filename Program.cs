10aa82dbdca3261118d1eb4bde88166c0fa56df2
Martin Pietschmann <schirkan86@gmx.de>
2026-02-22 23:01:16 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Blue Prism Prozess-Export Tool
+// Exportiert Blue Prism Prozesse mit AutomateC.exe CLI
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessExporter exporter = new ProcessExporter();
+        exporter.ExportProcess();
+    }
+}
